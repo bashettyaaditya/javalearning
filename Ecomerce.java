8e2c32818dfdee7d1f2bcef76850b740ec35a286
Aditya <bashe@BASHETTYAADITYA>
2026-07-17 09:29:57 +0530

    qwert

---
+++ Ecomerce.java
@@ -0,0 +1,18 @@
+public class Ecomerce{
+    private Product[] items = new Product[5];
+    private List<Product> itemList = new ArrayList<>();
+
+    public void array(addProduct) {
+    items.add(product);
+}
+}
+
+    //1. ACCESSING ELEMENTS
+    String first = students.get(0); //AA
+    String second = students.get(2); //BB(NAME)
+    System.out.println("First: " + first +", Third: "+ third);
+
+    //2. REMOVING THE ELEMENTS
+    students.remove(1); 
+    students.remove("BB"); //removes BB(by objects)
+    System.out.println("After removals: "+students);
